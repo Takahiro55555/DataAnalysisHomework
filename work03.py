787eb2574d744e01c3165b39899cd0f46b0e631f
Takahiro555555 <tkhr.corei7@gmail.com>
2019-04-27 01:31:57 +0900

    ミスを修正

--- work03.py
+++ work03.py
@@ -1,5 +1,7 @@
 '''
-    @author:Takahiro55555
+    @file: work03.py
+    @brief: 誤差伝搬の課題
+    @author: Takahiro55555
 '''
 import csv
 import math
@@ -10,7 +12,6 @@ def main():
         reader = csv.reader(f)
         data = [row for row in reader]
     del data[0] # CSVヘッダーの削除
-
     data_num = len(data) # データの行数
     
     # 合計を算出
@@ -18,7 +19,7 @@ def main():
     for row in data:
         sums["d1"] += float(row[1])
         sums["d2"] += float(row[2])
-        print(row)
+        # print(row)
 
     # 平均を算出
     averages = {}
@@ -28,8 +29,8 @@ def main():
     # 分散を算出
     dispersions = {"d1":0, "d2":0}
     for row in data:
-        dispersions["d1"] += (float(row[1]) - averages["d1"]) * (float(row[1]) - averages["d1"])
-        dispersions["d2"] += (float(row[2]) - averages["d2"]) * (float(row[2]) - averages["d2"])
+        dispersions["d1"] += math.pow(float(row[1]) - averages["d1"], 2)
+        dispersions["d2"] += math.pow(float(row[2]) - averages["d2"], 2)
     dispersions["d1"] /= data_num - 1
     dispersions["d2"] /= data_num - 1
 
@@ -45,23 +46,23 @@ def main():
 
     # d1とd2の長さの差の誤差
     diff_12 = averages["d1"] - averages["d2"]
-    varians = math.sqrt(dispersions["d1"] + dispersions["d2"])
+    varians = math.sqrt(math.pow(std_deviations_mean["d1"], 2) + math.pow(std_deviations_mean["d2"], 2))
 
     # 外周の長さ
     rect_length = 2 * (averages["d1"] + averages["d2"])
-    rect_varians = math.sqrt((2*std_deviations_mean["d1"])*(2*std_deviations_mean["d1"]) + (2*std_deviations_mean["d2"])*(2*std_deviations_mean["d2"]))
+    rect_varians = math.sqrt(math.pow(2*std_deviations_mean["d1"], 2) + math.pow(2*std_deviations_mean["d2"], 2))
 
     # 面積の算出
     rect_area = averages["d1"] * averages["d2"]
-    rect_area_varians = math.sqrt(math.pow(std_deviations_mean["d1"]*averages["d1"], 2) + math.pow(std_deviations_mean["d2"]*averages["d2"], 2))
+    rect_area_varians = math.sqrt(math.pow(std_deviations_mean["d1"]*averages["d2"], 2)\
+                        + math.pow(std_deviations_mean["d2"]*averages["d1"], 2))
 
     # 結果の表示
-    print("averages d1:{0}, d2:{1}".format(averages["d1"], averages["d2"]))
-    print("  d1 length:{0} +- {1}".format(averages["d1"], std_deviations_mean["d1"]))
-    print("  d2 length:{0} +- {1}".format(averages["d2"], std_deviations_mean["d2"]))
-    print("length diff:{0} +- {1}".format(diff_12 ,varians))
-    print("rect length:{0} +- {1}".format(rect_length, rect_varians))
-    print("  rect area:{0} +- {1}".format(rect_area, rect_area_varians))
+    print("{0:<34}:{1:>10.5f} +- {2:.5f}".format("d1 length", averages["d1"], std_deviations_mean["d1"]))
+    print("{0:<34}:{1:>10.5f} +- {2:.5f}".format("d2 length", averages["d2"], std_deviations_mean["d2"]))
+    print("{0:<34}:{1:>10.5f} +- {2:.5f}".format("difference in length of d1 and d2", diff_12, varians))
+    print("{0:<34}:{1:>10.5f} +- {2:.5f}".format("sides length (d1 + d2)*2", rect_length, rect_varians))
+    print("{0:<34}:{1:>10.5f} +- {2:.5f}".format("rect area", rect_area, rect_area_varians))
 
 if __name__ == '__main__':
     main()
